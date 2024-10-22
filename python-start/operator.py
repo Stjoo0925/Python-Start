09faf69983ef88a63afd9128b6335ad855f86e82
Stjoo0925 <stjoo0925@gmail.com>
2024-10-22 15:10:21 +0900

    feat: operator

---
+++ python-start/operator.py
@@ -0,0 +1,31 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2
+
+print(2**3) # 2^3 = 8
+print(5%3) # 나머지 2
+print(10%3) # 1
+print(5//3) # 몫 1
+print(10//3) # 3
+
+print(10>3) # True
+print(4 >= 7) # False
+print(10 < 3) #F alse
+print(5 <= 5) # True
+
+print(3 == 3) # 서로 같음 True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # 서도 다름 True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # AND 조건 True
+print((3 > 0) & (3 < 5)) # 마찬가지로 AND 조건 True
+
+print((3 > 0) or (3 > 5)) # OR 조건 True
+print((3 > 0) | (3 > 5)) # 마찬가지로 OR 조건 True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
