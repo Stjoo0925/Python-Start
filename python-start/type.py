a7df14cce73e61f4070503971acc93b916055b3f
Stjoo0925 <stjoo0925@gmail.com>
2024-10-22 14:39:05 +0900

    feat: type

---
+++ python-start/type.py
@@ -0,0 +1,26 @@
+# 숫자 자료형
+
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# 문자 자료형
+
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+
+# 참/거짓 자료형 (boolean)
+
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5>10))
