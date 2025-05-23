a191a6aefb70bf001d548a91312529c65721667c
Stjoo0925 <stjoo0925@gmail.com>
2025-05-23 23:57:21 +0900

    feat : 학습 진행중

--- crawling/3일차/main.py
+++ crawling/3일차/main.py
@@ -8,3 +8,23 @@
 
 # **동적 웹사이트 이해**
 # 브라우저에서 `https://www.reddit.com` 접속
+
+"""
+<a href="user?id=todsacerdoti" class="user">todsacerdoti</a>
+"""
+
+# **실습: robots.txt 확인**
+
+# 브라우저에서 `https://news.ycombinator.com/robots.txt` 접속
+
+# 크롤링 허용/제한 경로 확인 (예: `Disallow: /x?fnid=`)
+
+"""
+Disallow: /x?fnid=
+"""
+
+# **주의사항**
+
+# 요청 속도 제한
+
+# robots.txt를 준수해 허용된 페이지만 크롤링
